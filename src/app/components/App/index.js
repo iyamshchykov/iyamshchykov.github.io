e459ca77d491c5046c5ebe0c3c9654b4569ab9e5
Ihor Yamshchykov <ihor.yamshchykov@gmail.com>
2020-06-12 22:58:23 +0300

    test parcel

--- src/app/components/App/index.js
+++ src/app/components/App/index.js
@@ -2,10 +2,6 @@ import React from "react";
 import img from "~/assets/pengbrew_160x160.png";
 import "./App.scss";
 
-const App = () => (
-  <div>
-    <img src={img} />
-  </div>
-);
+const App = () => <img src={img} />;
 
 export default App;
